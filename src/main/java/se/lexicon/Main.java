2420a3a7c55d520ac72db026aa9b48653ea1dd44
Dennis Olsen <dennan_lol@hotmail.com>
2025-03-06 16:42:19 +0100

    First commit and idea text in Main.java

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,23 @@
+package se.lexicon;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+//Idea
+//Start with welcome screen with login only one hard coded account so far.
+//After logging in, show main menu, checking on credit, adding, taking away, show account info. Hard coded amounts.
+//Admin option of having a list of all users and some info about them. All hard coded users.
+
+//Class person, class people, class account
+//showing person info, showing all people, manipulate account.
+
+//Show all the different menu's, main menu, account menu, list of user menu.
+//taking input from user to either select different menu's or to add/remove from account.
+
+//Future ideas
+//Creating account option, only temp account until database
+//Sub menu's for the main menu.
+//Adding database for storing all accounts, info of the account and their credit.
