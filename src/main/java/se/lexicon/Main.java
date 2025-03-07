b0ebd7eaa879c2e1f510f19050838b9291f45517
Dennis Olsen <dennan_lol@hotmail.com>
2025-03-07 16:44:32 +0100

    First commit and idea text in Main.java

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -7,6 +7,22 @@ public class Main {
 }
 
 //Idea
+
+//How should I start if I want a web page to connect to java backend later?
+
+/* Från Navtinder Sagooo
+Backend: Java + Springboot. Springboot har massor med roliga grejer, typ spring security, database etc.
+Axios som du kan använda som ditt rest api. (Men kolla ifall det kommit annat som fungerar bättre)
+React.Js eller Vue.Js till Frontenden.
+
+DB = Springboot har ju grejer men undrar lite om man kan köra PostGreSQL istället
+
+KLura lite på också om du ska göra event driven programing (vilket det förmodligen lär bli,
+om jag inte är helt ute och cyklar men tänkte på det med gui:et då) innan du börjar skissa arkitekturen.
+Och om du vill göra det ännu störe då drar du in docker och kubernetes i det hela. Ja man kan spåra hur mycket som helst haha
+ */
+
+
 //Start with welcome screen with login only one hard coded account so far.
 //After logging in, show main menu, checking on credit, adding, taking away, show account info. Hard coded amounts.
 //Admin option of having a list of all users and some info about them. All hard coded users.
@@ -18,6 +34,7 @@ public class Main {
 //taking input from user to either select different menu's or to add/remove from account.
 
 //Future ideas
+//Creating a web page for it
 //Creating account option, only temp account until database
 //Sub menu's for the main menu.
 //Adding database for storing all accounts, info of the account and their credit.
